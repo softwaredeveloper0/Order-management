3dffb602e2de8242da6e8fb0455610428370e8b1
Developer <dev@localhost>
2026-05-17 13:40:47 +0530

    ci/cd added

--- inventoryservice/src/main/java/com/example/inventoryservice/InventoryEntity.java
+++ inventoryservice/src/main/java/com/example/inventoryservice/InventoryEntity.java
@@ -3,22 +3,22 @@ package com.example.inventoryservice;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Table;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
-import jakarta.persistence.Version;
-import lombok.*;
-
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -53,13 +53,9 @@ public class InventoryEntity {
     @PrePersist
     private void onCreate(){
 
-
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
 
         if(reservedStock == null) reservedStock = 0L;
-
-   
-
-
+    }
 }
