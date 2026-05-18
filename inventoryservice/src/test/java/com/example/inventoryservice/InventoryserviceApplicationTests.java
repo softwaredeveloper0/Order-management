d1c510328c80eadea8f11771d8e713750063e46f
Developer <dev@localhost>
2026-05-18 07:35:32 +0530

    changes

--- inventoryservice/src/test/java/com/example/inventoryservice/InventoryserviceApplicationTests.java
+++ inventoryservice/src/test/java/com/example/inventoryservice/InventoryserviceApplicationTests.java
@@ -2,11 +2,13 @@ package com.example.inventoryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
         "spring.rabbitmq.listener.simple.auto-startup=false",
         "spring.rabbitmq.listener.direct.auto-startup=false"
 })
+@ActiveProfiles("test")
 class InventoryserviceApplicationTests {
 
 	@Test
